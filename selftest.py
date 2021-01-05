b3b3e113055f2e62c44f848c669e4a1fc7e74353
명이 <76814120+myoung2namur@users.noreply.github.com>
2021-01-05 17:05:34 +0900

    log commit

--- selftest.py
+++ selftest.py
@@ -4,8 +4,34 @@ import os
 import datetime
 import random
 
+def errorLog(name,reason):
+    with open(name,"w",encoding='utf8') as f:
+        f.write("에러 : True\n")
+        f.write(f"이유 : {reason}\n")
+        f.close()
+
 
 def main():
+
+    now = datetime.datetime.now()
+    year = now.year
+    month = now.month
+    day = now.day
+    hour = now.hour
+    minute = now.minute
+    sec = now.second
+    rn = random.randint(1000, 10000)
+    if (month < 10):
+        month = f'0{month}'
+    if (day < 10):
+        day = f'0{day}'
+    if (hour < 10):
+        hour = f'0{hour}'
+    if (sec < 10):
+        sec = f'0{sec}'
+    formated = f'{year}{month}{day}{hour}{minute}{sec}{rn}.log'
+
+    
     if (os.path.exists("개인정보.yml") == False):
         with open("개인정보.yml", "w", encoding='utf8') as f:
             f.writelines("이름 : #이름\n")
@@ -17,6 +43,10 @@ def main():
             f.close()
             print("개인정보.yml 파일을 수정해주세요")
             input("프로그램을 종료하려면 ENTER 키를 눌러주세요.")
+            errorLog(formated,'''
+개인정보.yml이 생성되었습니다.
+개인정보.yml에 정보를 입력하고, 다시 실행시켜주세요.
+''')
             return
 
     with open("개인정보.yml", "r", encoding='utf8') as f:
@@ -35,26 +65,22 @@ def main():
         for param in lists:
             if (param == None):
                 input("개인정보.yml에 빈칸이 있습니다.\n프로그램을 종료하려면 ENTER 키를 눌러주세요")
+                errorLog(formated,'''
+개인정보.yml에 빈칸이 있습니다.
+빈칸을 모두 채우고 다시 실행시켜주세요.
+''')
                 return
 
         data = hcskr.selfcheck(name, birth, region, school,
                                level, password)
 
-        now = datetime.datetime.now()
-        year = now.year
-        month = now.month
-        day = now.day
-        hour = now.hour
-        minute = now.minute
-        sec = now.second
-        rn = random.randint(1000, 9999)
-        formated = f'{year}{month}{day}{hour}{minute}{sec}{rn}.log'
+
 
         with open(formated, "w", encoding='utf8') as f:
-            f.write(f"isError = {data['error']}\n")
-            f.write(f"Result = {data['code']}\n")
-            f.write(f"Message = {data['message']}\n")
-            f.write(f"Time = {data['regtime']}")
+            f.write(f"에러 = {data['error']}\n")
+            f.write(f"성공 여부 = {data['code']}\n")
+            f.write(f"세부 결과 = {data['message']}\n")
+            f.write(f"성공 시간 = {data['regtime']}")
             f.close()
 
 
